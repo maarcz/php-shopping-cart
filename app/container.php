3f3262a45d469ba49528f671ec85d3942d8a48cf
Marcis Zoldners <marcis.zoldners@gmail.com>
2017-09-12 00:04:41 +0300

    Setting up

---
+++ app/container.php
@@ -0,0 +1,22 @@
+<?php
+
+use Slim\Views\Twig;
+use Slim\Views\TwigExtension;
+use Interop\Container\ContainerInterface;
+use function DI\get;
+
+return [
+    'router' => get(Slim\Router::class),
+    Twig::class => function (ContainerInterface $c) {
+        $twig = new Twig(__DIR__ . '/../resources/views', [
+            'cache' => false
+        ]);
+
+        $twig->addExtension(new TwigExtension(
+            $c->get('router'),
+            $c->get('request')->getUri()
+        ));
+
+        return $twig;
+    }
+];
